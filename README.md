aa7571531d82df9544fbe07e11a3b8153e231fab
Jacob Novak <jnovak0021@pointloma.edu>
2025-01-30 07:32:09 -0800

    Jacob - integrated with USGS API --  need to work on efficiency of pagination and make it so that it does not overwhelm the server

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ To Run this application clone the repository.
 **This option is old but I am leaving just in case -- If nodemon works, just use that**
 Once the repository is stored locally on your machine:
 1. Navigate to the root directory of the project
-2. run the command **docker-compose up --build -d**
+2. run the command ** docker-compose up --build -d **
 
 This will containerize the full stack and will allow you to run the program.
 
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,6 @@
-import UserInterface  from '@/components/UserInterface';
-import Globe from '@/components/Globe';
+// import UserInterface  from '@/components/UserInterface';
+// import Globe from '@/components/Globe';
+import EarthQuakeFilter from '@/components/EarthqaukeFilter';
 
 
 export default function index() {
@@ -7,13 +8,15 @@ export default function index() {
     <div className="mb-6">
       <h3 className="text-lg font-semibold text-white text-center">Interactive Globe</h3>
       <div
-        className="overflow-auto max-h-96 border rounded-md"
+        className="max-h-96 border rounded-md"
         style={{ width: '100%', height: '300px' }}>
-        <Globe />
-        <h1>GLOBE</h1>
+        {/* <Globe /> */}
+        <EarthQuakeFilter />
+        <h1></h1>
+
         
       </div>
-      <UserInterface backendName="go"/>
+      {/* <UserInterface backendName="go"/> */}
     </div>
   );
 }

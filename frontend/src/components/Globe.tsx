5c11247517b44f95c480922e38a3f01767ca78f8
Jacob Novak <jnovak0021@pointloma.edu>
2025-01-30 14:44:24 -0800

    Globe commit

--- frontend/src/components/Globe.tsx
+++ frontend/src/components/Globe.tsx
@@ -17,6 +17,45 @@ const Globe: React.FC = () => {
         zoom: 13, // Initial map zoom level
       });
 
+      
+      mapRef.current.addControl(new mapboxgl.FullscreenControl({container: document.querySelector('body')}));
+
+      /*
+
+    for (const marker of geojson.features) {
+      const el = document.createElement('div');
+      const width = marker.properties.iconSize[0];
+      const height = marker.properties.iconSize[1];
+      el.className = 'marker';
+      el.style.backgroundImage = `url(https://picsum.photos/id/${marker.properties.imageId}/${width}/${height})`;
+      el.style.width = `${width}px`;
+      el.style.height = `${height}px`;
+      el.style.backgroundSize = '100%';
+      el.style.display = 'block';
+      el.style.border = 'none';
+      el.style.borderRadius = '50%';
+      el.style.cursor = 'pointer';
+      el.style.padding = 0;
+
+      el.addEventListener('click', () => {
+        window.alert(marker.properties.message);
+      });
+
+      new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(mapRef.current);
+    }
+      */ 
+
+
+
+      //https://docs.mapbox.com/mapbox-gl-js/api/markers/
+      // Create a new marker.
+      /*
+      const marker = new mapboxgl.Marker()
+         .setLngLat([30.5, 50.5])
+         .addTo(mapRef.current);
+      */
       return () => {
         if (mapRef.current) {
           mapRef.current.remove();
